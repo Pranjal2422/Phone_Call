ec298b08b96ac4fe6c0a59971a34171467ce4136
Pranjal Tulsiyan <pranjaltulsiyan1@gmail.com>
2022-10-12 20:40:07 +0530

    Full Project

---
+++ app/src/main/java/com/activity/activitytoactivity/MainActivity.java
@@ -0,0 +1,60 @@
+package com.activity.activitytoactivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.telephony.SmsManager;
+import android.text.Editable;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button b1 = findViewById(R.id.button1);
+        Button call = findViewById(R.id.call);
+        EditText phone = findViewById(R.id.call_no);
+
+        //Function to call other activity
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(),MainActivity2.class);
+                startActivity(i);
+            }
+        });
+
+        //Function to call a phone number
+        call.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String phone_no = phone.getText().toString();
+                Intent intent = new Intent(Intent.ACTION_CALL);
+                intent.setData(Uri.parse("tel:" + phone_no));
+                startActivity(intent);
+            }
+        });
+
+        // Function to send sms
+        Button sms = findViewById(R.id.sms);
+        EditText smsph = findViewById(R.id.smsph);
+        EditText smsmssg = findViewById(R.id.smsmssg);
+        sms.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//               Intent i = new Intent(Intent.ACTION_SEND);
+                SmsManager smsManager = SmsManager.getDefault();
+                String phone = smsph.getText().toString();
+                String message = smsmssg.getText().toString();
+                smsManager.sendTextMessage(phone,null,message,null,null);
+            }
+        });
+    }
+}
